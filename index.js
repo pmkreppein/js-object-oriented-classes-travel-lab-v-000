2ae96be1f7b0ee93acd03d1c42cea761f6708503
Peter Kreppein <pmkreppein@gmail.com>
2019-01-11 05:56:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let verDistance = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)
+    let horDistance = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    return verDistance + horDistance
+  };
+
+  estimatedTime(peak) {
+    return peak == true ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3;
+  }
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
